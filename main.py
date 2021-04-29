a226dc61d633eddc21ff189c3051d250e5dea5a3
Michael Pruitt <anthony.m.pruitt@gmail.com>
2021-04-29 14:47:56 -0500

    Remove GUI window. Refactoring.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from pdf_merge import run
+import sys
 
+from pdf_merge import run
 
 if __name__ == '__main__':
     run()
+    sys.exit(0)
--- pdf_merge/app.py
+++ pdf_merge/app.py
@@ -1,12 +1,53 @@
-from dataclasses import dataclass
+import os
+import sys
+from pathlib import Path
+from tkinter.messagebox import showerror, showinfo
+from typing import Iterable
 
-from .gui import get_gui
+from PyPDF4 import PdfFileMerger
+from PyPDF4.utils import PdfReadError, PdfReadWarning
 
-
-@dataclass
-class App:
-    gui = get_gui()
+from pdf_merge.gui import get_gui, GUI
 
 
 def run():
-    return App()
+    gui = get_gui()
+    merger = _prompt_and_get_merged_input_files(gui)
+    output_path = _prompt_get_output_path(gui)
+    _write_merged_pdf(merger, output_path)
+    os.startfile(output_path.as_posix())
+    sys.exit(0)
+
+
+def _get_merger_from_paths(paths=Iterable[Path]) -> PdfFileMerger:
+    merger = PdfFileMerger()
+    for p in paths:
+        with p.open(mode="rb") as fo:
+            try:
+                merger.append(fo)
+            except PdfReadError:
+                showerror("Error Reading PDF File", f"Unable to read PDF file:\n{fo.name}")
+    if not merger.pages:
+        showerror("No Files Selected", "No files were selected.")
+        sys.exit(0)
+    return merger
+
+
+def _prompt_get_output_path(gui: GUI) -> Path:
+    if not (p := gui.ask_get_output_file_text_buffer()):
+        showinfo("No Output Path Selected", "No output path was selected.\nCanceling merge.")
+        sys.exit(0)
+    return Path(p.name)
+
+
+def _write_merged_pdf(merger: PdfFileMerger, path: Path) -> None:
+    with path.open("wb") as fo:
+        merger.write(fo)
+
+
+def _prompt_get_input_files(gui: GUI) -> Iterable[Path]:
+    return (Path(p) for p in gui.ask_get_input_file_paths())
+
+
+def _prompt_and_get_merged_input_files(gui: GUI) -> PdfFileMerger:
+    return _get_merger_from_paths(_prompt_get_input_files(gui))
